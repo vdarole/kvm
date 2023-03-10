e22e1312d66b76b995e51175189827729d925fcc
Vallabh Darole <66929597+vdarole@users.noreply.github.com>
2023-03-10 17:37:55 +0530

    Update kvm02-start-stop-vm.sh

--- kvm02-start-stop-vm.sh
+++ kvm02-start-stop-vm.sh
@@ -2,8 +2,8 @@
 # Purpose: To Start and Shutdown the VMS
 # Version: 1.0
 # Created Date: 7 Sept 2022
-# Modified Date: 
-# Author : 
+# Modified Date:  6 March 2023
+# Author : Vallabh Darole
 
 echo
 echo "1. START ALL VIRTUAL MACHINE "
@@ -12,27 +12,39 @@ echo "3. Quit"
 echo "Please select the option:"
 read STATUS
 
-echo "VM are " $STATUS `date` >> /root/vm-logs.txt 
+echo "VM are " $STATUS `date` >> /root/vm-logs.txt
 
 case $STATUS in
-"1") 
+
+## Below tasks is to start the VM
+
+"1")
 virsh start web01
 sleep 2
 virsh start db01
 sleep 2
 virsh start ans01
 
+## Below to check the status
+virsh list
+
 ;;
-"2") 
+
+## Below tasks are to shutdown the VM
+
+"2")
 virsh shutdown web01
 sleep 2
 virsh shutdown db01
 sleep 2
 virsh shutdown ans01
+
+## Below to check the status
+virsh list --all
+
 ;;
 "3") exit
 ;;
 *) echo "$STATUS is not a valid option"
 ;;
 esac
-
